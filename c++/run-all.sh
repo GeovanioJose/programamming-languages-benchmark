7be384f493cce1c9734cec1be9ed0f81cf114990
GeovanioJose <sgeojose@gmail.com>
2026-07-02 17:14:45 -0300

    C++

---
+++ c++/run-all.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+cd binarytrees
+./binarytrees.sh
+cd ..
+
+sleep 2m
+
+cd fannkuchredux
+./fannkuchredux.sh
+cd ..
+
+sleep 2m
+
+cd fasta
+./fasta.sh
+cd ..
+
+sleep 2m
+
+cd k-nucleotide
+./knucleotide.sh
+cd ..
+
+sleep 2m
+
+cd mandelbrot
+./mendelbrot.sh
+cd ..
+
+sleep 2m
+
+cd nbody
+./nbody.sh
+cd ..
+
+sleep 2m
+
+cd pidigits
+./pidigits.sh
+cd ..
+
+sleep 2m
+
+cd regexredux
+./regexredux.sh
+cd ..
+
+sleep 2m
+
+cd reverse-complement
+./revcomp.sh
+cd ..
+
+sleep 2m
+
+cd spectralnorm
+./spectralnorm.sh
+cd ..
+
+poweroff
+
+
+
+
+
+
+
+
+
+
+
