696f07f83f23f175f7990e19429a3bc3c6ac59e3
GeovanioJose <sgeojose@gmail.com>
2026-07-02 17:31:46 -0300

    Javascript

---
+++ javascript/run-all.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+cd binarytrees
+./binarytrees.sh
+cd ..
+
+sleep 2m
+
+cd fannkuchredux
+./fannkuchredux.sh
+cd ..
+
+sleep 2m
+
+cd fasta
+./fasta.sh
+cd ..
+
+sleep 2m
+
+cd k-nucleotide
+./knucleotide.sh
+cd ..
+
+sleep 2m
+
+cd mandelbrot
+./mendelbrot.sh
+cd ..
+
+sleep 2m
+
+cd n-body
+./nbody.sh
+cd ..
+
+sleep 2m
+
+cd pidigits
+./pidigits.sh
+cd ..
+
+sleep 2m
+
+cd regex-redux
+./regexredux.sh
+cd ..
+
+sleep 2m
+
+cd reverse-complement
+./revcomp.sh
+cd ..
+
+sleep 2m
+
+cd spectral-norm
+./spectralnorm.sh
+cd ..
+
+poweroff
+
+
+
+
+
+
+
+
+
+
+
